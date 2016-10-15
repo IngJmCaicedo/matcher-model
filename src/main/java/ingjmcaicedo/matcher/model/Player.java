7b7d204dcdc733601b58f7c7d07ec5498c326cdb
IngJmCaicedo <jmcaicedo1@gmail.com>
2016-10-15 13:40:12 -0500

    Initial commit

---
+++ src/main/java/ingjmcaicedo/matcher/model/Player.java
@@ -0,0 +1,31 @@
+package ingjmcaicedo.matcher.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Player {
+
+	@Id
+	private String id;
+	private String document;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDocument() {
+		return document;
+	}
+	public void setDocument(String document) {
+		this.document = document;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/ingjmcaicedo/matcher/model/Team.java
@@ -0,0 +1,24 @@
+package ingjmcaicedo.matcher.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Team {
+
+	@Id
+	private String id;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
